6b0d37a5c05188115a3f85e875c77870a783992f
MikM22 <56986647+MikM22@users.noreply.github.com>
2023-09-28 11:23:08 -0500

    Add files via upload

--- solution.cpp
+++ solution.cpp
@@ -1,92 +1,78 @@
 /**
  * CSCI 241
  *
- * Authors: Mik Mieczkowski (z2010367), Nasiru Williams (z1907156), Anna Madalone (z1997286)
+ * Author: Mik Mieczkowski (z2010367)
  *
- * solution.cpp
+ *
  */
-
 #include <iostream>
-#include <cmath>
 #include <iomanip>
+#include "solution.h"
+using std::cin;
+using std::cout;
+using std::endl;
 
-using namespace std;
-
-int minimum(int array[], int n);
-int maximum(int array[], int n);
-int range(int array[], int n);
-double average(int array[], int n);
-double median(int array[], int n);
-double stddev(int array[], int n);
-void sort_array(int array[], int n);
-
-int minimum(int array[], int n){
-    sort_array(array, n);
-    return array[0];
-}
-int maximum(int array[], int n){
-    sort_array(array, n);
-    return array[n-1];
-}
-int range(int array[], int n){
-    return maximum(array, n) - minimum(array,n);
-}
-double average(int array[], int n){
-    double avg;
-    double total;
-    for(int i=0;i<n;i++){
-        total += array[i];
-    }
-    avg = total / n;
-    return avg;
-}
-double median(int array[], int n){
-    if (n%2==0) {
-        return (array[n/2-1] + array[n/2])/2;
-    } else {
-        return array[n/2];
-    }
-}
-double stddev(int array[], int n){
-    int sqDist;
-    int avg = average(array, n);
-    for (int i = 0; i < n; i++) {
-        sqDist += (array[i] - avg) * (array[i] - avg);
-    }
-    return sqrt(sqDist/n);
-}
-
-/**
- * Sorts a list of integers in ascending order.
- *
- * @param array The list of integers to sort.
- * @param n The size of the list.
- *
- * @note Uses the selection sort algorithm.
- */
-void sort_array(int array[], int n)
+solution::solution()
 {
-    int i, j, min_index;
-    // int temp;
-
-    for (i = 0; i < n - 1; i++)
-    {
-        // Locate the smallest value in the unsorted part of the list.
-
-        min_index = i;
-        for (j = i + 1; j < n; j++)
-            if (array[j] < array[min_index])
-                min_index = j;
-
-        // Swap smallest value with first unsorted value.
-
-        swap(array[i], array[min_index]);
-
-        /*
-        temp = array[i];
-        array[i] = array[min_index];
-        array[min_index] = temp;
-        */
-    }
+    for (int row = 0; row < 9; row++)
+        for (int col = 0; col < 9; col++)
+            grid[row][col] = 0;
 }
 
+void solution::read()
+{
+    for (int row = 0; row < 9; row++)
+        for (int col = 0; col < 9; col++)
+            cin >> grid[row][col];
+}
+   
+void solution::print() const
+{
+    for (int row = 0; row < 9; row++)
+    {
+        for (int col = 0; col < 9; col++)
+            cout << grid[row][col] << ' ';
+
+        cout << endl;
+    }
+
+    cout << endl;
+}
+bool solution::verify() const {
+    //r
+    for (int r = 0; r < 9; r++) {
+        bool num[9] = {false};
+        for (int c = 0; c < 9; c++) {
+            if (num[grid[r][c]-1]) {
+                return false;
+            } else {
+                num[grid[r][c]-1] = true;
+            }
+        }
+    }
+    for (int c = 0; c < 9; c++) {
+        bool num[9] = {false};
+        for (int r = 0; r < 9; r++) {
+            if (num[grid[r][c]-1]) {
+                return false;
+            } else {
+                num[grid[r][c]-1] = true;
+            }
+        }
+    }
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            bool num[9] = {false};
+            for (int k = 0; k < 3; k++) {
+                for (int l = 0; l < 3; l++) {
+                    if (num[grid[i*3+k][j*3+l]-1]) {
+                        return false;
+                    } else {
+                        num[grid[i*3+k][j*3+l]-1] = true;
+                    }
+                }
+            }
+        }
+    }
+    return true;
+}
